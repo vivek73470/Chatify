b6415235329cdf94f92bea6ba3b9e828c7a463ad
vivek73470 <vivek734706@gmail.com>
2026-01-30 16:28:07 +0530

    message socket

--- Backend/socket/message.socket.js
+++ Backend/socket/message.socket.js
@@ -1,13 +1,12 @@
 export default function messageHandlers(io, socket, onlineUsers) {
-  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-    const receiverSocketId = onlineUsers.get(receiverId);
+socket.on("sendMessage", (message) => {
+    const { receiverId } = message;
 
-    if (receiverSocketId) {
-      io.to(receiverSocketId).emit("receiveMessage", {
-        senderId,
-        text,
-        createdAt: new Date(),
-      });
-    }
-  });
+const receiverSocketId = onlineUsers.get(message.receiver);
+
+if (receiverSocketId) {
+  io.to(receiverSocketId).emit("receiveMessage", message);
 }
+
+  });
+};
